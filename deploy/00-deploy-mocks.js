bdb34bcb14a774bb0c73de915968c74c21ce670a
Ben A <benjamin.anoufa@gmail.com>
2022-09-23 11:19:02 +0200

    deployement done

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -2,13 +2,13 @@ const { network } = require("hardhat");
 const { developmentChains } = require("../helper-hardhat-config");
 
 const BASE_FEE = ethers.utils.parseEther("0.25"); //0.25 is the premium. It costs 0.25 LINK.
-const GAZ_PRICE_LINK = 1e9; // calculate value
+const GAS_PRICE_LINK = 1e9; // calculate value
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
     const chainId = network.config.chainId;
-    const arg = [BASE_FEE, GAS_PRICE_LINK];
+    const args = [BASE_FEE, GAS_PRICE_LINK];
 
     if (developmentChains.includes(network.name)) {
         log("Local network detected! Deploying Mocks...");
